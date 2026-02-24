ae8d2e33b3d956717432180b454284ace574f9fa
Sudchana Punyakunwanit <>
2026-02-24 17:19:06 +0700

    update cron

--- app/scheduler.py
+++ app/scheduler.py
@@ -260,6 +260,6 @@ def check_reminders():
         db.close()
 
 scheduler = BackgroundScheduler()
-# scheduler.add_job(check_reminders, 'cron', hour=10, minute=0) # Run everyday at 10:00 AM
+scheduler.add_job(check_reminders, 'cron', hour=10, minute=0) # Run everyday at 10:00 AM
 # scheduler.add_job(check_reminders, 'interval', hours=24) # Run once a day
-scheduler.add_job(check_reminders, 'interval', seconds=10) # For testing
+# scheduler.add_job(check_reminders, 'interval', seconds=10) # For testing
